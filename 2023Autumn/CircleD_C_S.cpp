751109beae516da474ae0a2734f9ce451b4685f2
llllllll11l <136985779+llllllll11l@users.noreply.github.com>
2023-06-22 13:17:27 +0800

    Create CircleD_C_S.cpp

---
+++ 2023Autumn/CircleD_C_S.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double PI=3.14159265;
+double r;
+
+int main()
+{
+	cin>>r;
+	cout<<"diameter:"<<r*2<<endl
+		<<"circumference:"<<r*2*PI<<endl
+		<<"square:"<<r*r*PI<<endl;
+	return 0;
+}
